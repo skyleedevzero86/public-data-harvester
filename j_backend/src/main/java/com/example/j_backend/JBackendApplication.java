9c51a7264ac4062faba3a178b52a134f8d2b6d2a
이경용 <sleekydz86@naver.com>
2025-03-31 23:28:10 +0900

    feat: project basic

---
+++ j_backend/src/main/java/com/example/j_backend/JBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.j_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JBackendApplication.class, args);
+    }
+
+}
---
+++ j_backend/src/test/java/com/example/j_backend/JBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
