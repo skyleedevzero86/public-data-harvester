ba20951ec2422a69a3c0d2b583abc87d8d58c8e1
이경용 <sleekydz86@icloud.com>
2025-06-30 21:42:15 +0900

    test: csv reader 테스트 코드 입력

--- j_backend/src/test/java/com/antock/service/CsvServiceTest.java
+++ j_backend/src/test/java/com/antock/service/CsvServiceTest.java
@@ -1,4 +1,56 @@
 package com.antock.service;
 
+import com.antock.api.coseller.application.dto.BizCsvInfo;
+import com.antock.api.coseller.application.dto.CsvService;
+import com.antock.api.coseller.value.City;
+import com.antock.api.coseller.value.District;
+import com.antock.global.common.constants.CsvConstants;
+import org.assertj.core.api.Assertions;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
 public class CsvServiceTest {
+
+    @Autowired
+    private CsvService csvService;
+
+    private City city;
+    private District district;
+    @BeforeEach
+    void setUp() {
+        city = City.valueOf("서울특별시");
+        district = District.valueOf("강남구");
+
+    }
+
+    @Test
+    @DisplayName("csv 파일 조회 검증")
+    public void read_csv_file_test() throws Exception {
+        //given
+
+        //when
+        long startTime =  System.currentTimeMillis();
+        List<BizCsvInfo> list = csvService.readBizCsv(city.name(), district.name());
+
+        long endTime =  System.currentTimeMillis();
+        System.out.println("소요시간 = " + (endTime - startTime) + " ms");
+        //then
+        assertNotNull(list);
+
+        BizCsvInfo info = list.get(0);
+        Assertions.assertThat(CsvConstants.CORP_TYPE_BIZ).isEqualTo(info.getBizType());
+    }
 }
