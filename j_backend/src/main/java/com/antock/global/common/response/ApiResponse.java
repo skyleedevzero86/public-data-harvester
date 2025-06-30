a65fab127c9fac635979f86b584600bb60772f19
이경용 <sleekydz86@icloud.com>
2025-06-30 21:25:25 +0900

    feat: api 공통 클래스 생성

--- j_backend/src/main/java/com/antock/global/common/response/ApiResponse.java
+++ j_backend/src/main/java/com/antock/global/common/response/ApiResponse.java
@@ -1,4 +1,41 @@
 package com.antock.global.common.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+/**
+ * API응답 공통 처리
+ * @param <T>
+ */
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int resultCode;
+    private String resultMsg;
+    private T data; //응답데이터
+
+    public ApiResponse(HttpStatus resultStatus, String resultMsg) {
+        this.resultCode = resultStatus.value();
+        this.resultMsg = resultMsg;
+    }
+
+    public ApiResponse(HttpStatus resultStatus, String resultMsg, T data) {
+        this.resultCode = resultStatus.value();
+        this.resultMsg = resultMsg;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus resultStatus, T data) {
+        return new ApiResponse<>(resultStatus, resultStatus.getReasonPhrase(), data);
+    }
+
+
+
 }
