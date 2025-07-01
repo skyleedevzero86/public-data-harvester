6f54bb6aa5a5db78313e2b8e653ba4067058afcb
이경용 <sleekydz86@icloud.com>
2025-07-01 10:22:40 +0900

    feat: CSV 조회 Service Test 구현

--- j_backend/src/test/java/com/antock/service/CsvServiceTest.java
+++ j_backend/src/test/java/com/antock/service/CsvServiceTest.java
@@ -5,17 +5,25 @@ import com.antock.api.coseller.application.dto.api.BizCsvInfoDto;
 import com.antock.api.coseller.value.City;
 import com.antock.api.coseller.value.District;
 import com.antock.global.common.constants.CsvConstants;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * CSV파일 조회 및 관리 Service Test
+ */
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = "csv.file-template=%s_%s.csv")
+@Import(CsvService.class)
 @SpringBootTest
 public class CsvServiceTest {
 
@@ -47,6 +55,17 @@ public class CsvServiceTest {
         assertNotNull(list);
 
         BizCsvInfoDto info = list.get(0);
-        Assertions.assertThat(CsvConstants.CORP_TYPE_BIZ).isEqualTo(info.getBizType());
+        assertThat(CsvConstants.CORP_TYPE_BIZ).isEqualTo(info.getBizType());
+    }
+
+    @Test
+    @DisplayName("csv 없는경우 DEFAULT CSV로 동일 시나리오 진행")
+    void readBizCsv_no_File() {
+        // 없는 경로의 파일 요청
+        List<BizCsvInfoDto> list = csvService.readBizCsv("대구광역시", "강남구");
+
+        BizCsvInfoDto info = list.get(0);
+        assertThat(CsvConstants.CORP_TYPE_BIZ).isEqualTo(info.getBizType());
+        assertThat(info.getBizNm()).isEqualTo("주식회사 뮤직터빈");
     }
 }
